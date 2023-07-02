e2f2f40d8aad11994d0ad40287cb5258a6f5480f
anh20042001 <114937681+anh20042001@users.noreply.github.com>
2023-07-02 17:10:32 +0700

    commit

---
+++ Day 4 - Variable data type and Operators/demo/asset/js/main.js
@@ -0,0 +1,19 @@
+// let comfirmatio = confirm("do you want to confirm your password?")
+function showMessage(){
+    alert('Xin chào!');
+}
+
+// let yourName = prompt("Hãy nhập tên của bạn");
+// document.write("Xin chào " + yourName);
+
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
